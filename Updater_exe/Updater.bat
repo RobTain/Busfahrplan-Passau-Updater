f7c2edcfd50f3e65d27a7a9a3a303349411951ac
RobTain <koenigro@fim.uni-passau.de>
2016-03-23 14:59:55 +0100

    Update Updater.bat

--- Updater_exe/Updater.bat
+++ Updater_exe/Updater.bat
@@ -1,3 +1,3 @@
-REM mit Systemstart ausführen (Autostart)  (shell:startup)
+REM mit Systemstart ausführen (Autostart)  (shell:startup, verknüpfung erstellen)
 cd F:\Busfahrplan\Updater_exe
 java -jar Updater.jar
